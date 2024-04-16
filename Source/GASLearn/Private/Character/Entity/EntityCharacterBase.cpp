064f72f503c7d99860b02549dfb1d4ec0c06ea82
obsidian-zero <63155026+obsidian-zero@users.noreply.github.com>
2024-04-16 22:43:37 +0800

    增添了非玩家角色基类（ASC直接保存在Character上）
    PlayerState只提供给玩家，导致非玩家角色应该使用另一套机制保存ASC

---
+++ Source/GASLearn/Private/Character/Entity/EntityCharacterBase.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Character/Entity/EntityCharacterBase.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+AEntityCharacterBase::AEntityCharacterBase(const class FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.SetDefaultSubobjectClass<UCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
+{
+	// 创建能力系统组件
+	EntityAbilitySystemComponent = CreateDefaultSubobject<UCharacterAbilitySystemComponent>(TEXT("EntityAbilitySystemComponent"));
+    
+	// 创建属性集组件
+	EntityAttributeSetBase = CreateDefaultSubobject<UCharacterAttributeSetBase>(TEXT("EntityAttributeSetBase"));
+}
+
+void AEntityCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AbilitySystemComponent = Cast<UCharacterAbilitySystemComponent>(EntityAbilitySystemComponent);
+	
+	AttributeSetBase = EntityAttributeSetBase;
+
+	EntityAbilitySystemComponent->InitAbilityActorInfo(this, this);
+
+	InitialAttruibutes();
+	SetHandSize(0);
+	SetSpellSlots(0);
+}
---
+++ Source/GASLearn/Public/Character/Entity/EntityCharacterBase.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GASLearn/GAS/Character/Ability/CharacterAbilitySystemComponent.h"
+#include "GASLearn/GAS/Attributes/CharacterAttributeSetBase.h"
+#include "Character/GASCharacterBase.h"
+#include "EntityCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASLEARN_API AEntityCharacterBase : public AGASCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AEntityCharacterBase();
+
+	AEntityCharacterBase(const class FObjectInitializer& ObjectInitializer);
+	
+	UPROPERTY()
+	class UCharacterAbilitySystemComponent * EntityAbilitySystemComponent;
+
+	UPROPERTY()
+	class UCharacterAttributeSetBase * EntityAttributeSetBase;
+
+	void BeginPlay() override;
+	
+};
